168a505d60e61bb3c6b23b6a087c5148b9068638
md ansari <saddamansari52@gmail.com>
2021-02-07 04:22:21 -0500

    updating IP address for Assignment2 sprint4 2nd attemp

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://3.250.52.32:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://54.194.122.146:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
