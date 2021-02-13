5d1c82d007c537f305f70aef2b30d44afd968524
md ansari <saddamansari52@gmail.com>
2021-02-13 07:00:04 -0500

    updating IP address for Assignment1 sprint5 1st  attemp

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://3.135.240.84:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://3.14.29.158:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
